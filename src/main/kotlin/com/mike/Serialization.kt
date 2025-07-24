00daa8b9881f4d36bc68eac5ab6bcda93ff4dee9
mikesplore <mikepremium8@gmail.com>
2025-07-24 08:11:20 +0300

    Fixed the json serialization issue

--- src/main/kotlin/com/mike/Serialization.kt
+++ src/main/kotlin/com/mike/Serialization.kt
@@ -12,7 +12,11 @@ import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
@@ -20,6 +24,24 @@ fun Application.configureSerialization() {
             // Configure Gson with pretty printing and lenient parsing
             serializeNulls()
             disableHtmlEscaping()
+            registerTypeAdapter(LocalDateTime::class.java, object : TypeAdapter<LocalDateTime>() {
+                private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+                override fun write(out: JsonWriter, value: LocalDateTime?) {
+                    if (value == null) {
+                        out.nullValue()
+                    } else {
+                        out.value(value.format(formatter))
+                    }
+                }
+                override fun read(`in`: JsonReader): LocalDateTime? {
+                    return if (`in`.peek() == com.google.gson.stream.JsonToken.NULL) {
+                        `in`.nextNull()
+                        null
+                    } else {
+                        LocalDateTime.parse(`in`.nextString(), formatter)
+                    }
+                }
+            })
         }
     }
     
