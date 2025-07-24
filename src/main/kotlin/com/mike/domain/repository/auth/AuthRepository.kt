7209786bb9766a826993a6e62288567958c764a6
mikesplore <mikepremium8@gmail.com>
2025-07-24 12:05:57 +0300

    fixed the change password

--- src/main/kotlin/com/mike/domain/repository/auth/AuthRepository.kt
+++ src/main/kotlin/com/mike/domain/repository/auth/AuthRepository.kt
@@ -1,12 +1,13 @@
 package com.mike.domain.repository.auth
 
+import com.mike.domain.model.auth.ChangePasswordRequest
 import com.mike.domain.model.auth.LoginCredentials
 import com.mike.domain.model.auth.RefreshToken
 import com.mike.domain.model.auth.TokenPayload
 import com.mike.domain.model.user.*
 
 interface AuthRepository {
-    fun changePassword(id: String, newPassword: String): Pair<Boolean, String?>
+    fun changePassword(changePasswordRequest: ChangePasswordRequest): Pair<Boolean, String?>
     fun login(loginCredentials: LoginCredentials): Triple<Profile?, TokenPayload?, String?> // Profile, accessToken, refreshToken
     fun createRefreshToken(userId: Int): Unit
     fun getRefreshToken(token: String): RefreshToken?
